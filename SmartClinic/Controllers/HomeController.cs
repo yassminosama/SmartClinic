949d9488027283db603a7b2f8588df286d63c305
Mohamed Mohsen <moodymohsen46@gmail.com>
2025-05-04 18:36:14 +0200

    paypal gateway

--- SmartClinic/Controllers/HomeController.cs
+++ SmartClinic/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using PayPal.Api;
 using SmartClinic.Models;
 
-namespace SmartClinic.Controllers
+
+namespace testGateWay.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private IHttpContextAccessor httpContextAccessor;
+        IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IHttpContextAccessor context, IConfiguration iconfiguration)
         {
             _logger = logger;
+            httpContextAccessor = context;
+            _configuration = iconfiguration;
         }
 
         public IActionResult Index()
@@ -18,15 +23,140 @@ namespace SmartClinic.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public ActionResult PaymentWithPaypal(string Cancel = null, string blogId = "", string PayerID = "", string guid = "")
         {
-            return View();
+            //getting the apiContext  
+            var ClientID = _configuration.GetValue<string>("PayPal:Key");
+            var ClientSecret = _configuration.GetValue<string>("PayPal:Secret");
+            var mode = _configuration.GetValue<string>("PayPal:mode");
+            APIContext apiContext = PaypalConfiguration.GetAPIContext(ClientID, ClientSecret, mode);
+
+            try
+            {
+                //A resource representing a Payer that funds a payment Payment Method as paypal  
+                //Payer Id will be returned when payment proceeds or click to pay  
+                string payerId = PayerID;
+                if (string.IsNullOrEmpty(payerId))
+                {
+                    //this section will be executed first because PayerID doesn't exist  
+                    //it is returned by the create function call of the payment class  
+                    // Creating a payment  
+                    // baseURL is the url on which paypal sendsback the data.  
+                    string baseURI = this.Request.Scheme + "://" + this.Request.Host + "/Home/PaymentWithPayPal?";
+                    //here we are generating guid for storing the paymentID received in session  
+                    //which will be used in the payment execution  
+                    var guidd = Convert.ToString((new Random()).Next(100000));
+                    guid = guidd;
+                    //CreatePayment function gives us the payment approval url  
+                    //on which payer is redirected for paypal account payment  
+                    var createdPayment = this.CreatePayment(apiContext, baseURI + "guid=" + guid, blogId);
+                    //get links returned from paypal in response to Create function call  
+                    var links = createdPayment.links.GetEnumerator();
+                    string paypalRedirectUrl = null;
+                    while (links.MoveNext())
+                    {
+                        Links lnk = links.Current;
+                        if (lnk.rel.ToLower().Trim().Equals("approval_url"))
+                        {
+                            //saving the payapalredirect URL to which user will be redirected for payment  
+                            paypalRedirectUrl = lnk.href;
+                        }
+                    }
+                    // saving the paymentID in the key guid  
+                    httpContextAccessor.HttpContext.Session.SetString("payment", createdPayment.id);
+                    return Redirect(paypalRedirectUrl);
+                }
+                else
+                {
+                    // This function exectues after receving all parameters for the payment  
+
+                    var paymentId = httpContextAccessor.HttpContext.Session.GetString("payment");
+                    var executedPayment = ExecutePayment(apiContext, payerId, paymentId as string);
+                    //If executed payment failed then we will show payment failure message to user  
+                    if (executedPayment.state.ToLower() != "approved")
+                    {
+
+                        return View("PaymentFailed");
+                    }
+                    var blogIds = executedPayment.transactions[0].item_list.items[0].sku;
+
+
+                    return View("PaymentSuccess");
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("PaymentFailed");
+            }
+           
+            return View("SuccessView");
+        }
+        private PayPal.Api.Payment payment;
+        private Payment ExecutePayment(APIContext apiContext, string payerId, string paymentId)
+        {
+            var paymentExecution = new PaymentExecution()
+            {
+                payer_id = payerId
+            };
+            this.payment = new Payment()
+            {
+                id = paymentId
+            };
+            return this.payment.Execute(apiContext, paymentExecution);
+        }
+        private Payment CreatePayment(APIContext apiContext, string redirectUrl, string blogId)
+        {
+            //create itemlist and add item objects to it  
+
+            var itemList = new ItemList()
+            {
+                items = new List<Item>()
+            };
+
+            itemList.items.Add(new Item()
+            {
+                name = "Item Detail",
+                currency = "USD",
+                price = "1.00",
+                quantity = "1",
+                sku = "asd"
+            });
+            var payer = new Payer()
+            {
+                payment_method = "paypal"
+            };
+            // Configure Redirect Urls here with RedirectUrls object  
+            var redirUrls = new RedirectUrls()
+            {
+                cancel_url = redirectUrl + "&Cancel=true",
+                return_url = redirectUrl
+            };
+
+            var amount = new Amount()
+            {
+                currency = "USD",
+                total = "1.00", // Total must be equal to sum of tax, shipping and subtotal.  
+                //details = details
+            };
+            var transactionList = new List<Transaction>();
+            // Adding description about the transaction  
+            transactionList.Add(new Transaction()
+            {
+                description = "Transaction description",
+                invoice_number = Guid.NewGuid().ToString(), //Generate an Invoice No  
+                amount = amount,
+                item_list = itemList
+            });
+            this.payment = new Payment()
+            {
+                intent = "sale",
+                payer = payer,
+                transactions = transactionList,
+                redirect_urls = redirUrls
+            };
+            // Create a payment using a APIContext  
+            return this.payment.Create(apiContext);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
